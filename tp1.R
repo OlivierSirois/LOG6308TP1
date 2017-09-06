66366d34360dac08c02119f683b0a757fe6d6f48
Alexandre BENTO <alexandre.bento@insa-lyon.fr>
2017-09-06 14:48:32 -0400

    Question 2 avec le cosinus

--- tp1.R
+++ tp1.R
@@ -1,27 +1,43 @@
 # Load libraries
 library("readr")
 library("dplyr")
+library("Matrix")
 
 # Open files
 data <- read.csv("u.data", sep='|')
 items <- read.csv("u.item.csv", sep='|')
 users <- read.csv("u.user.csv", sep='|')
 
-# Question 1
-users_jobs <- users %>% select(id, job) %>% group_by(job)
-ratings <- data %>% select(user.id, rating)
+# ----------------------------- Question 1 -------------------------------------
+# users_jobs <- users %>% select(id, job) %>% group_by(job)
+# ratings <- data %>% select(user.id, rating)
+#
+# users_avg_ratings <- data %>% group_by(user.id) %>% summarize(avg_rating = mean(rating))
+#
+# users_avg_ratings <- inner_join(users_avg_ratings, rename(users, user.id = id))
+#
+# ratings_by_job <- users_avg_ratings %>% group_by(job) %>% summarize(avg_rating = mean(avg_rating))
+#
+# ratings_by_job
+#
+# ratings_by_age <- users_avg_ratings %>% group_by(age) %>% summarize(avg_rating = mean(avg_rating))
+#
+# ratings_by_age
 
-users_avg_ratings <- data %>% group_by(user.id) %>% summarize(avg_rating = mean(rating))
+# ----------------------------- Question 2 -------------------------------------
+ratings <- sparseMatrix(i = data[,1], j = data[,2], x = data[,3])
 
-users_avg_ratings <- inner_join(users_avg_ratings, rename(users, user.id = id))
+## Cosinus entre un vecteur v et chaque colonne dela matrice m
+cosinus.vm <- function(v,m) { n <- sqrt(colSums(m^2)); (v %*% m)/(n * sqrt(sum(v^2))) }
 
-ratings_by_job <- users_avg_ratings %>% group_by(job) %>% summarize(avg_rating = mean(avg_rating))
+# Trouve les indexes des premières 'n' valeurs maximales d'une matrice
+max.nindex <- function(m, n=5) {
+    i <- order(m, decreasing=TRUE)
+    return(i[1:n])
+}
 
-ratings_by_job
+ratings_cos <- cosinus.vm(ratings[,450], ratings)
 
-ratings_by_age <- users_avg_ratings %>% group_by(age) %>% summarize(avg_rating = mean(avg_rating))
+closest_movies <- max.nindex(ratings_cos)
 
-ratings_by_age
-
-# Question 2
-# ratings <- data %>% sparseMatrix(i = )
+closest_movies
